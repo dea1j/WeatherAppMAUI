9cf848215a249bb6f7857daee5e11d4da9362cdd
dea1j <jdayo2012@gmail.com>
2023-06-02 17:22:29 +0100

    initial commit

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapApiKey = "e172d45c2ed0f06f891f49dd27c5959f";
+    }
+}
